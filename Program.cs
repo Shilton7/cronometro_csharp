4d918dccbf33d0c22af35641c7240a828e0d1c9f
shilton <shilton.santiago@animaeducacao.com.br>
2020-10-23 10:06:40 -0300

    Criando o projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace stopwatch_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
