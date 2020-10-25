d54966eb1dab071ac1fa77790979d8eedffb8e91
shilton <shilton.santiago@animaeducacao.com.br>
2020-10-25 20:56:53 -0300

    Criação do Menu e uso do Substring

--- Program.cs
+++ Program.cs
@@ -7,12 +7,28 @@ namespace stopwatch_csharp
   {
     static void Main(string[] args)
     {
-      start();
+      Menu();
     }
 
-    static void start()
+    static void Menu()
     {
-      int time = 10;
+      Console.Clear();
+      Console.WriteLine("S = Segundos (Ex: 7s = 7 segundos");
+      Console.WriteLine("M = Minutos (Ex: 7m = 7 minutos");
+      Console.WriteLine("0 = Sair");
+      Console.WriteLine("Quanto tempo deseja contar ?");
+
+      string data = Console.ReadLine().ToLower();
+      char type = char.Parse(data.Substring(data.Length - 1, 1));
+      int time = int.Parse(data.Substring(0, data.Length - 1));
+
+      Console.WriteLine(time);
+      Console.WriteLine(type);
+
+    }
+
+    static void Start(int time, char type)
+    {
       int currentTime = 0;
 
       while (currentTime != time)
@@ -22,6 +38,10 @@ namespace stopwatch_csharp
         Console.WriteLine(currentTime);
         Thread.Sleep(1000);
       }
+
+      Console.Clear();
+      Console.WriteLine("Stopwatch finalizado!");
+      Thread.Sleep(3000);
 
     }
   }
